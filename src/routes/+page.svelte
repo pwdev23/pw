a09313283fa899e533e348e99b4e35b752afdcba
Irhas <muhammadirhasalh@gmail.com>
2025-02-11 16:54:00 +0700

    refactor: init daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
+<div class="navbar bg-base-100">
+  <a href="/" class="btn btn-ghost text-xl">Project Weekend</a>
+</div>
 <div class="hero bg-base-200 min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Hi there</h1>
-      <p class="py-6">This site is under development.</p>
+      <h1 class="text-5xl font-bold">Coming Soon</h1>
+      <p class="py-6">We're building something.</p>
+      <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
     </div>
   </div>
 </div>
